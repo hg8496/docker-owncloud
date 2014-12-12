660856febbfa02a52bf6ddba947269d92b69e131
Christian Stolz <hg8496@cstolz.de>
2014-12-12 21:35:10 +0100

    One should replace ALL palceholder

--- rights.sh
+++ rights.sh
@@ -1,10 +1,10 @@
 chown -R root:root /var/www/html/
 chmod -R 755 /var/www/html/
-chown <http-user>:<http-user> /var/www/html/config/config.php
+chown www-data:www-data /var/www/html/config/config.php
 chmod 750 /var/www/html/config/config.php
-chown -R <http-user>:<http-user> /var/www/html/data/
+chown -R www-data:www-data /var/www/html/data/
 chmod -R 750 /var/www/html/data
 chown root:root /var/www/html/data/.htaccess
 chmod 755 /var/www/html/data/.htaccess
-chown <http-user>:<http-user> /var/www/html/apps/
+chown www-data:www-data /var/www/html/apps/
 chmod 750 /var/www/html/apps/
